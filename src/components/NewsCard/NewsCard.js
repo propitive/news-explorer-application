e1e206dad14879f9048058e002d60fe982b3f38b
Propitive <propitive@gmail.com>
2023-07-19 08:50:28 -0500

    working on news cards media query

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,45 +1,62 @@
-import { useState } from "react"
-import bookmarked from "../../images/bookmarked.svg"
-import dogOnTree from "../../images/dogOnTree.png"
-import notBookmarked from "../../images/notBookmarked.svg"
+import { useState } from "react";
+import bookmarked from "../../images/bookmarked.svg";
+import dogOnTree from "../../images/dogOnTree.png";
+import notBookmarked from "../../images/notBookmarked.svg";
 
 function NewsCard(card, index) {
-    console.log(index === 1)
-    const [isHovering, setIsHovering] = useState(-1)
+  console.log(index === 1);
+  const [isHovering, setIsHovering] = useState(-1);
 
-    const cardSigninClassname = `card__signin ${isHovering === card.index ? "" : "hidden"}`
+  const cardSigninClassname = `card__signin ${
+    isHovering === card.index ? "" : "hidden"
+  }`;
+  const cardPublisherArray = card.card.source.name.split(" ");
+  const cardPublisher = cardPublisherArray[0];
+  console.log(cardPublisher);
 
-    const renderNotBookmarked = () => {
-        return (
-            <div> 
-                <button className="card__button">
-                    <img className="card__bookmark" src={notBookmarked}></img>
-                </button>
-            </div>
-        )
-    }
-
-    const renderBookmarked = () => {
-        return (
-            <button className="card__button">
-                <img className="card__bookmark" src={bookmarked} onMouseEnter={() => setIsHovering(card.index)} onMouseLeave={() => setIsHovering(-1)}></img>
-            </button>
-        )
-    }
-
+  const renderNotBookmarked = () => {
     return (
-        <div className="card">
-            <img className="card__image" src={card.card.urlToImage} alt={card.title} />
-            <h3 className="card__date">{card.card.publishedAt.slice(0,9)}</h3>
-            <h2 className="card__title">{card.card.title}</h2>
-            <h3 className="card__paragraph" id="js-toclamp">{card.card.description}</h3>
-            <h3 className="card__publisher">{card.card.source.name}</h3>
-            <div className={cardSigninClassname}>
-                <h3>Sign in to save articles</h3>
-            </div>
-            {renderNotBookmarked()}
-        </div>
-    )
+      <div>
+        <button className="card__button">
+          <img className="card__bookmark" src={notBookmarked}></img>
+        </button>
+      </div>
+    );
+  };
+
+  const renderBookmarked = () => {
+    return (
+      <button className="card__button">
+        <img
+          className="card__bookmark"
+          src={bookmarked}
+          onMouseEnter={() => setIsHovering(card.index)}
+          onMouseLeave={() => setIsHovering(-1)}
+        ></img>
+      </button>
+    );
+  };
+
+  return (
+    <div className="card">
+      {console.log(card.card.source.name.split(" ")[0])}
+      <img
+        className="card__image"
+        src={card.card.urlToImage}
+        alt={card.title}
+      />
+      <h3 className="card__date">{card.card.publishedAt.slice(0, 9)}</h3>
+      <h2 className="card__title">{card.card.title}</h2>
+      <h3 className="card__paragraph" id="js-toclamp">
+        {card.card.description}
+      </h3>
+      <h3 className="card__publisher">{card.card.source.name.split(" ")[0]}</h3>
+      <div className={cardSigninClassname}>
+        <h3>Sign in to save articles</h3>
+      </div>
+      {renderNotBookmarked()}
+    </div>
+  );
 }
 
 export default NewsCard;
--- src/components/SearchResultsHome/SearchResultsHome.js
+++ src/components/SearchResultsHome/SearchResultsHome.js
@@ -15,50 +15,54 @@ function SearchResultsHome({ visible, showMoreItems }) {
 
   return (
     <div className="search">
-      <h2 className="search__title">Search results</h2>
-      <ul className={searchCardsClassname}>
-        {cardItems.articles.slice(0, visible).map((card, index) => (
-          <div className="card" key={index}>
-            <img
-              className="card__image"
-              src={card.urlToImage}
-              alt={card.title}
-            />
-            <h3 className="card__date">{card.publishedAt.slice(0, 9)}</h3>
-            <h2 className="card__title">{card.title}</h2>
-            <h3 className="card__paragraph" id="js-toclamp">
-              {card.description}
-            </h3>
-            <h3 className="card__publisher">{card.source.name}</h3>
-            <div
-              className={`${
-                isHovering === index ? "card__signin" : "card__signin__hidden"
-              }`}
-            >
-              <h3>Sign in to save articles</h3>
+      <div className="search__container">
+        <h2 className="search__title">Search results</h2>
+        <ul className={searchCardsClassname}>
+          {cardItems.articles.slice(0, visible).map((card, index) => (
+            <div className="card" key={index}>
+              <img
+                className="card__image"
+                src={card.urlToImage}
+                alt={card.title}
+              />
+              <h3 className="card__date">{card.publishedAt.slice(0, 9)}</h3>
+              <h2 className="card__title">{card.title}</h2>
+              <h3 className="card__paragraph" id="js-toclamp">
+                {card.description}
+              </h3>
+              <h3 className="card__publisher">
+                {card.source.name.split(" ")[0]}
+              </h3>
+              <div
+                className={`${
+                  isHovering === index ? "card__signin" : "card__signin__hidden"
+                }`}
+              >
+                <h3>Sign in to save articles</h3>
+              </div>
+              <div>
+                <button className="card__button">
+                  {currentUser === null ? (
+                    <img
+                      className="card__bookmark"
+                      src={notBookmarked}
+                      onMouseEnter={() => setIsHovering(index)}
+                      onMouseLeave={() => setIsHovering(-1)}
+                    ></img>
+                  ) : (
+                    <img className="card__bookmark" src={notBookmarked}></img>
+                  )}
+                </button>
+              </div>
             </div>
-            <div>
-              <button className="card__button">
-                {currentUser === null ? (
-                  <img
-                    className="card__bookmark"
-                    src={notBookmarked}
-                    onMouseEnter={() => setIsHovering(index)}
-                    onMouseLeave={() => setIsHovering(-1)}
-                  ></img>
-                ) : (
-                  <img className="card__bookmark" src={notBookmarked}></img>
-                )}
-              </button>
-            </div>
-          </div>
-        ))}
-      </ul>
-      {visible === 3 && (
-        <button className="search__button" onClick={showMoreItems}>
-          Show more
-        </button>
-      )}
+          ))}
+        </ul>
+        {visible === 3 && (
+          <button className="search__button" onClick={showMoreItems}>
+            Show more
+          </button>
+        )}
+      </div>
     </div>
   );
 }
