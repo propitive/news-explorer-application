dcbe04198e0aae95160b2e3dbb5da86ac4a238f2
Jose M. Maldonado <jmaldonado@associa.us>
2023-07-21 16:02:11 -0500

    continue to work on profile navbar

--- src/components/NavBarProfile/NavBarProfile.js
+++ src/components/NavBarProfile/NavBarProfile.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import CurrentUserContext from "../../context/CurrentUserContext";
 import logoutIconBlack from "../../images/logoutIconBlack.svg";
+import toggleButtonBlack from "../../images/toggle-button-black.svg"
+import closeButton from "../../images/close-button.svg";
 import { Link } from "react-router-dom";
 
 function NavBarProfile({ handleVisibleReset }) {
   const currentUser = React.useContext(CurrentUserContext);
-  console.log(handleVisibleReset);
+  const [isMenuHomeToggled, setIsMenuProfileToggled] = useState(false);
+
+  const handleToggleMenuHomeAppear = () => {
+    setIsMenuProfileToggled(true);
+  };
+  const handleToggleMenuHomeDisappear = () => {
+    setIsMenuProfileToggled(false);
+  };
+
+  const handleLogOut = () => {
+
+  }
 
   return (
-    <section className="navbar-profile">
+    <>
+      <section className="navbar-profile">
       <Link to="/" style={{ textDecoration: "none", alignSelf: "center" }}>
         <h1
           className="navbar-profile__title"
@@ -18,6 +32,9 @@ function NavBarProfile({ handleVisibleReset }) {
           NewsExplorer
         </h1>
       </Link>
+
+      <img className="navbar-profile__toggle-button" src={toggleButtonBlack} onClick={handleToggleMenuHomeAppear}/>
+
       <div className="navbar-profile__buttons">
         <div className="navbar-profile__home-container">
           <Link to="/" style={{ textDecoration: "none", alignSelf: "center" }}>
@@ -46,6 +63,23 @@ function NavBarProfile({ handleVisibleReset }) {
         </button>
       </div>
     </section>
+          {isMenuHomeToggled && (
+            <section className="menu">
+              <div className="menu-profile__top">
+                <h1 className="menu-profile__title">NewsExplorer</h1>
+                <img
+                  className="menu-profile__close-icon"
+                  src={closeButton}
+                  onClick={handleToggleMenuHomeDisappear}
+                />
+              </div>
+              <div className="menu-profile__bottom">
+                <h2 className="menu-profile__home">Home</h2>
+                <button className="menu-profile__signin" onClick={handleLogOut} >Sign in</button>
+              </div>
+            </section>
+          )}
+    </>
   );
 }
 
