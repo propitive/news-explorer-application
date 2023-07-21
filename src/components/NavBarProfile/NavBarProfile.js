c20484c52e85d5ccfafbbd0f169780627d18dca0
Propitive <propitive@gmail.com>
2023-07-21 17:45:49 -0500

    completed navbar profile responsive

--- src/components/NavBarProfile/NavBarProfile.js
+++ src/components/NavBarProfile/NavBarProfile.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import CurrentUserContext from "../../context/CurrentUserContext";
+import logoutIcon from "../../images/logoutIcon.svg";
 import logoutIconBlack from "../../images/logoutIconBlack.svg";
-import toggleButtonBlack from "../../images/toggle-button-black.svg"
+import toggleButtonBlack from "../../images/toggle-button-black.svg";
 import closeButton from "../../images/close-button.svg";
 import { Link } from "react-router-dom";
 
@@ -16,69 +17,96 @@ function NavBarProfile({ handleVisibleReset }) {
     setIsMenuProfileToggled(false);
   };
 
-  const handleLogOut = () => {
-
-  }
+  const handleLogOut = () => {};
 
   return (
     <>
       <section className="navbar-profile">
-      <Link to="/" style={{ textDecoration: "none", alignSelf: "center" }}>
-        <h1
-          className="navbar-profile__title"
-          onClick={handleVisibleReset}
-          id="home"
-        >
-          NewsExplorer
-        </h1>
-      </Link>
+        <Link to="/" style={{ textDecoration: "none", alignSelf: "center" }}>
+          <h1
+            className="navbar-profile__title"
+            onClick={handleVisibleReset}
+            id="home"
+          >
+            NewsExplorer
+          </h1>
+        </Link>
 
-      <img className="navbar-profile__toggle-button" src={toggleButtonBlack} onClick={handleToggleMenuHomeAppear}/>
+        <img
+          className="navbar-profile__toggle-button"
+          src={toggleButtonBlack}
+          onClick={handleToggleMenuHomeAppear}
+        />
 
-      <div className="navbar-profile__buttons">
-        <div className="navbar-profile__home-container">
-          <Link to="/" style={{ textDecoration: "none", alignSelf: "center" }}>
-            <button
-              className="navbar-profile__home"
-              onClick={handleVisibleReset}
+        <div className="navbar-profile__buttons">
+          <div className="navbar-profile__home-container">
+            <Link
+              to="/"
+              style={{ textDecoration: "none", alignSelf: "center" }}
             >
-              Home
-            </button>
-          </Link>
-          <button
-            className="navbar-profile__white-line"
-            disabled="disabled"
-          ></button>
+              <button
+                className="navbar-profile__home"
+                onClick={handleVisibleReset}
+              >
+                Home
+              </button>
+            </Link>
+            <button
+              className="navbar-profile__white-line"
+              disabled="disabled"
+            ></button>
+          </div>
+          <button className="navbar-profile__saved-articles">
+            Saved Articles
+          </button>
+          <button className="navbar-profile__logout">
+            {currentUser}
+            <img
+              className="navbar-profile__image"
+              src={logoutIconBlack}
+              alt="Logout icon"
+            />
+          </button>
         </div>
-        <button className="navbar-profile__saved-articles">
-          Saved Articles
-        </button>
-        <button className="navbar-profile__logout">
-          {currentUser}
-          <img
-            className="navbar-profile__image"
-            src={logoutIconBlack}
-            alt="Logout icon"
-          />
-        </button>
-      </div>
-    </section>
-          {isMenuHomeToggled && (
-            <section className="menu">
-              <div className="menu-profile__top">
-                <h1 className="menu-profile__title">NewsExplorer</h1>
-                <img
-                  className="menu-profile__close-icon"
-                  src={closeButton}
-                  onClick={handleToggleMenuHomeDisappear}
-                />
-              </div>
-              <div className="menu-profile__bottom">
-                <h2 className="menu-profile__home">Home</h2>
-                <button className="menu-profile__signin" onClick={handleLogOut} >Sign in</button>
-              </div>
-            </section>
-          )}
+      </section>
+      {isMenuHomeToggled && (
+        <section className="menu">
+          <div className="menu-profile__top">
+            <h1 className="menu-profile__title">NewsExplorer</h1>
+            <img
+              className="menu-profile__close-icon"
+              src={closeButton}
+              onClick={handleToggleMenuHomeDisappear}
+            />
+          </div>
+          <div className="menu-profile__bottom">
+            <Link
+              to="/"
+              style={{
+                textDecoration: "none",
+                alignSelf: "center",
+                width: "100%",
+              }}
+            >
+              <button
+                className="menu-profile__home"
+                onClick={handleVisibleReset}
+              >
+                Home
+              </button>
+            </Link>
+            <h2 className="menu-profile__saved">Saved Articles</h2>
+            <button className="menu-profile__signin">
+              {currentUser}
+              <img
+                className="navbar-profile__image"
+                src={logoutIcon}
+                alt="Logout icon"
+              />
+            </button>
+          </div>
+        </section>
+      )}
     </>
   );
 }
