f72e1bd3e0db9781a4d9995ea51922e22d2f58f3
Propitive <propitive@gmail.com>
2023-07-28 17:29:17 -0500

    css corrections on abuot component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -13,14 +13,9 @@ function About() {
         <p className="about__paragraphs">
           In a galaxy far far away... Just kidding, unfortunately, I am no Jedi!
           My name is Jose Maldonado, I am an aspiring software engineer
-          currently working in the IT industry. I am confident with HTML, CSS,
-          JavaScript, Git, Figma, React.js, Rest API, Node.js, Express.js,
-          MongoDB, and Google Cloud. <br></br> <br></br>
-          Throughout the past nine months, I have been tirelessly working to
-          ensure I can confidently tackle any web development issue thrown my
-          way. Moreover, the mentorship of industry veterans along with the
-          difficult challenges I have had to overcome have drastically increased
-          my ability to push through adversity and always find the solution.{" "}
+          currently working in the IT industry. <br></br> <br></br>I am
+          confident with HTML, CSS, JavaScript, Git, Figma, React.js, Rest API,
+          Node.js, Express.js, MongoDB, and Google Cloud.{" "}
         </p>
       </div>
     </section>
